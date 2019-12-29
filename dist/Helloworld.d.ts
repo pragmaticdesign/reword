0ccdfe7d50e946627bb4eceb56f4f33389fc9963
James Cook <jamespetercook@googlemail.com>
2019-12-29 15:57:09 +0000

    Not exporting type info (temporarily)

--- dist/Helloworld.d.ts
+++
@@ -1,4 +0,0 @@
-import * as React from 'react';
-declare const HelloWorld: React.SFC;
-export default HelloWorld;
-//# sourceMappingURL=HelloWorld.d.ts.map
--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { default as HelloWorld } from './HelloWorld';
-export { WordpressAPI } from './services/WordpressAPI';
-export { Posts } from './services/Posts';
-//# sourceMappingURL=index.d.ts.map
